5230a53f49aef3612ac9e6e01febe4f8fab49bf9
github-action-benchmark <github@users.noreply.github.com>
2026-03-04 21:58:41 +0000

    add AutoInstrument Benchmarks (benchmarkdotnet) benchmark result for fc8bc47cdf74ac53d828fc1be45eab2451abc9ad

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1772144081140,
+  "lastUpdate": 1772661521593,
   "repoUrl": "https://github.com/CallumTeesdale/AutoInstrument",
   "entries": {
     "AutoInstrument Benchmarks": [
@@ -72,6 +72,78 @@ window.BENCHMARK_DATA = {
             "value": 1.1778499443943684,
             "unit": "ns",
             "range": "± 0.00948365376927015"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "fc8bc47cdf74ac53d828fc1be45eab2451abc9ad",
+          "message": "Bump Scalar.AspNetCore from 2.12.48 to 2.12.54 (#10)\n\n---\nupdated-dependencies:\n- dependency-name: Scalar.AspNetCore\n  dependency-version: 2.12.54\n  dependency-type: direct:production\n  update-type: version-update:semver-patch\n...\n\nSigned-off-by: dependabot[bot] <support@github.com>\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2026-03-04T21:48:15Z",
+          "tree_id": "0827cabc2d17fee4b4b2b896f1280eaa768661b6",
+          "url": "https://github.com/CallumTeesdale/AutoInstrument/commit/fc8bc47cdf74ac53d828fc1be45eab2451abc9ad"
+        },
+        "date": 1772661521342,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "AutoInstrument.Benchmarks.InstrumentationOverheadBenchmarks.AsyncTask_Baseline",
+            "value": 0.3144940970731633,
+            "unit": "ns",
+            "range": "± 0.0036012886138790286"
+          },
+          {
+            "name": "AutoInstrument.Benchmarks.InstrumentationOverheadBenchmarks.AsyncTask_Instrumented",
+            "value": 9.90019930366959,
+            "unit": "ns",
+            "range": "± 0.012598159200868068"
+          },
+          {
+            "name": "AutoInstrument.Benchmarks.InstrumentationOverheadBenchmarks.AsyncTaskOfT_Baseline",
+            "value": 6.547764061673267,
+            "unit": "ns",
+            "range": "± 0.27256761947765784"
+          },
+          {
+            "name": "AutoInstrument.Benchmarks.InstrumentationOverheadBenchmarks.AsyncTaskOfT_Instrumented",
+            "value": 22.178974567457686,
+            "unit": "ns",
+            "range": "± 0.8699313990679937"
+          },
+          {
+            "name": "AutoInstrument.Benchmarks.InstrumentationOverheadBenchmarks.SyncReturn_Baseline",
+            "value": 0.0010796025661485536,
+            "unit": "ns",
+            "range": "± 0.0018623621515123118"
+          },
+          {
+            "name": "AutoInstrument.Benchmarks.InstrumentationOverheadBenchmarks.SyncReturn_Instrumented",
+            "value": 1.2083543429949455,
+            "unit": "ns",
+            "range": "± 0.005069994210633264"
+          },
+          {
+            "name": "AutoInstrument.Benchmarks.InstrumentationOverheadBenchmarks.SyncVoid_Baseline",
+            "value": 0.0007396036138137182,
+            "unit": "ns",
+            "range": "± 0.0015012565096095866"
+          },
+          {
+            "name": "AutoInstrument.Benchmarks.InstrumentationOverheadBenchmarks.SyncVoid_Instrumented",
+            "value": 1.1739970050752162,
+            "unit": "ns",
+            "range": "± 0.004139864861435986"
           }
         ]
       }
